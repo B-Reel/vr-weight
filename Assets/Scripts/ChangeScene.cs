fd8302eb70dd9a5d837b0f30fe6b559c797da5a3
mcxsic <michel.comin.escude@gmail.com>
2017-05-30 16:10:14 -0700

    adding init state

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -6,26 +6,56 @@ using System.Collections;
 public class ChangeScene : MonoBehaviour {
 
     public string SceneToLoad;
+    public int timerDetectController;
+    bool rightDetected = false;
+    bool leftDetected = false;
 
     // Use this for initialization
     void Start () {
-	
-	}
+        timerDetectController = 0;
+    }
 	
 	// Update is called once per frame
 	void Update () {
         var allTrackedObjects = FindObjectsOfType<SteamVR_TrackedObject>();
-        
+
+        int rightIndex = SteamVR_Controller.GetDeviceIndex(SteamVR_Controller.DeviceRelation.Rightmost);
+        int leftIndex = SteamVR_Controller.GetDeviceIndex(SteamVR_Controller.DeviceRelation.Leftmost);
+//        uint leftIndex2 = OpenVR.System.GetTrackedDeviceIndexForControllerRole(ETrackedContro‌​llerRole.LeftHand);
+  //      uint rightIndex2 = OpenVR.System.GetTrackedDeviceIndexForControllerRole(ETrackedContro‌​llerRole.RightHand);
+
         // Let's start by finding our controllers
         foreach (var trackedObject in allTrackedObjects)
         {
+            Debug.Log((int)trackedObject.index);
             var system = OpenVR.System;
             if (system != null && system.GetTrackedDeviceClass((uint)trackedObject.index) == ETrackedDeviceClass.Controller)
             {
                 var device = SteamVR_Controller.Input((int)trackedObject.index);
                 if (device != null && device.GetPressDown(EVRButtonId.k_EButton_Grip))
-                    SceneManager.LoadScene(SceneToLoad);
+                {
+                    rightDetected |= (int)trackedObject.index == rightIndex;
+                    leftDetected |= (int)trackedObject.index == leftIndex;
+                    timerDetectController = 15;
+                }
+                  
             }       
         }
+
+        if (timerDetectController > 0)
+        {
+            timerDetectController--;
+            if (rightDetected && leftDetected)
+            {
+                SceneManager.LoadScene(SceneToLoad);
+                timerDetectController = 0;
+            }
+
+            if (timerDetectController == 0)
+            {
+                rightDetected = false;
+                leftDetected = false;
+            }
+        } 
     }
 }
---
+++ Assets/Scripts/IntroBehavior.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IntroBehavior : MonoBehaviour {
+    public GameObject tables;
+    public GameObject basketHoop;
+    private float rotationFinal;
+    private float rotationEased;
+
+    private Vector3 position;
+    private Quaternion rotation;
+    private Vector3 rotationAngles;
+
+    // Use this for initialization
+    void Start () {
+        tables.SetActive(false);
+        basketHoop.SetActive(false);
+        rotationFinal = Camera.main.transform.rotation.eulerAngles.y;
+        rotationEased = rotationFinal;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(Camera.main.transform.position);
+
+        position = transform.position;
+        position.x = Camera.main.transform.position.x;
+        position.z = Camera.main.transform.position.z;
+        position.y = 1.0f;
+        transform.position = position;
+
+
+        rotationFinal = Camera.main.transform.rotation.eulerAngles.y;
+        rotationEased = Mathf.LerpAngle(rotationEased, rotationFinal, Time.deltaTime * 4);
+
+        rotation = transform.rotation;
+        rotationAngles = rotation.eulerAngles;
+        rotationAngles.y = rotationEased;
+        rotationAngles.x = 0;
+        rotationAngles.z = 0;
+        rotation.eulerAngles = rotationAngles;
+        transform.rotation = rotation;
+    }
+}
