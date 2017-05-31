e86ee67383226bd1983504359fc0005e4c428a0c
mcxsic <michel.comin.escude@gmail.com>
2017-05-30 18:18:03 -0700

    Added double trigger to remove init state, double grip to reset scene

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -6,17 +6,46 @@ using System.Collections;
 public class ChangeScene : MonoBehaviour {
 
     public string SceneToLoad;
-    public int timerDetectController;
+    public GameObject fadeOutGO;
+
+    public bool updatingScene = false;
+
+    int timerDetectController;
+    Renderer fadeOutRenderer;
     bool rightDetected = false;
     bool leftDetected = false;
+
+    bool loadScene = false;
+    bool initScene = true;
+
+    float elapsedTime = 0;
 
     // Use this for initialization
     void Start () {
         timerDetectController = 0;
+        fadeOutRenderer = fadeOutGO.GetComponent<Renderer>();
+        updateAlpha(1);
     }
 	
 	// Update is called once per frame
 	void Update () {
+        updatingScene = loadScene | initScene;
+
+        if (loadScene || initScene)
+        {
+            elapsedTime += Time.deltaTime * 0.9f;
+            updateAlpha(initScene ? 1-elapsedTime : elapsedTime);
+            if (elapsedTime >= 1) {
+                elapsedTime = 0f;
+                initScene = false;
+
+                if (loadScene)
+                    SceneManager.LoadScene(SceneToLoad);
+            }
+                
+            return;
+        }
+
         var allTrackedObjects = FindObjectsOfType<SteamVR_TrackedObject>();
 
         int rightIndex = SteamVR_Controller.GetDeviceIndex(SteamVR_Controller.DeviceRelation.Rightmost);
@@ -47,7 +76,7 @@ public class ChangeScene : MonoBehaviour {
             timerDetectController--;
             if (rightDetected && leftDetected)
             {
-                SceneManager.LoadScene(SceneToLoad);
+                loadScene = true;
                 timerDetectController = 0;
             }
 
@@ -57,5 +86,18 @@ public class ChangeScene : MonoBehaviour {
                 leftDetected = false;
             }
         } 
+    }
+
+    private void updateAlpha(float position)
+    {
+        Color c = fadeOutRenderer.material.color;
+        c.a =QuartEaseInOut(Mathf.Clamp(position, 0, 1), 0, 1, 1);
+        fadeOutRenderer.material.color = c;
+    }
+
+    private float QuartEaseInOut(float t, float b, float c, float d)
+    {
+        if ((t /= d / 2) < 1) return c / 2 * t * t * t * t + b;
+        return -c / 2 * ((t -= 2) * t * t * t - 2) + b;
     }
 }
--- Assets/Scripts/IntroBehavior.cs
+++ Assets/Scripts/IntroBehavior.cs
@@ -1,9 +1,20 @@
 ﻿using UnityEngine;
+using Valve.VR;
 using System.Collections;
 
 public class IntroBehavior : MonoBehaviour {
     public GameObject tables;
     public GameObject basketHoop;
+    public GameObject introUi;
+    public GameObject introEnvironment;
+    public GameObject fadeToBlack;
+    public ChangeScene changeScene;
+
+    int timerDetectController;
+    Renderer fadeRenderer;
+    bool rightDetected = false;
+    bool leftDetected = false;
+
     private float rotationFinal;
     private float rotationEased;
 
@@ -11,34 +22,141 @@ public class IntroBehavior : MonoBehaviour {
     private Quaternion rotation;
     private Vector3 rotationAngles;
 
+    private bool removeIntro = false;
+    private bool introVisible = true;
+    float elapsedTime = 0;
+
+    private float easedRotation;
+
     // Use this for initialization
     void Start () {
         tables.SetActive(false);
         basketHoop.SetActive(false);
+        introUi.SetActive(true);
+        introEnvironment.SetActive(true);
         rotationFinal = Camera.main.transform.rotation.eulerAngles.y;
         rotationEased = rotationFinal;
+
+        timerDetectController = 0;
+        fadeRenderer = fadeToBlack.GetComponent<Renderer>();
+        updateAlpha(0);
     }
 	
 	// Update is called once per frame
 	void Update () {
-        Debug.Log(Camera.main.transform.position);
+        if (introVisible)
+        {
+            Debug.Log(Camera.main.transform.position);
 
-        position = transform.position;
-        position.x = Camera.main.transform.position.x;
-        position.z = Camera.main.transform.position.z;
-        position.y = 1.0f;
-        transform.position = position;
+            /* Update intro position. */
+            position = transform.position;
+            position.x = Camera.main.transform.position.x;
+            position.z = Camera.main.transform.position.z;
+            position.y = 1.0f;
+            transform.position = position;
+
+            /* Update intro eased rotation. */
+            rotationFinal = Camera.main.transform.rotation.eulerAngles.y;
+            rotationEased = Mathf.LerpAngle(rotationEased, rotationFinal, Time.deltaTime * 4);
+
+            /* Update intro rotation. */
+            rotation = transform.rotation;
+            rotationAngles = rotation.eulerAngles;
+            rotationAngles.y = rotationEased;
+            rotationAngles.x = 0;
+            rotationAngles.z = 0;
+            rotation.eulerAngles = rotationAngles;
+            transform.rotation = rotation;
 
 
-        rotationFinal = Camera.main.transform.rotation.eulerAngles.y;
-        rotationEased = Mathf.LerpAngle(rotationEased, rotationFinal, Time.deltaTime * 4);
+            /* Update ui rotation. */
+            //Quaternion qUI = introUi.transform.localRotation;
+            //Vector3 rotUI = qUI.eulerAngles;
+            //rotUI.x = Mathf.LerpUnclamped(120, 60, Camera.main.transform.position.y * 0.5f);
+            //qUI.eulerAngles = rotUI;
+            //introUi.transform.localRotation = qUI;
 
-        rotation = transform.rotation;
-        rotationAngles = rotation.eulerAngles;
-        rotationAngles.y = rotationEased;
-        rotationAngles.x = 0;
-        rotationAngles.z = 0;
-        rotation.eulerAngles = rotationAngles;
-        transform.rotation = rotation;
+
+            if (!removeIntro && !changeScene.updatingScene)
+            {
+                /* Check if we need to remove intro. */
+                var allTrackedObjects = FindObjectsOfType<SteamVR_TrackedObject>();
+
+                int rightIndex = SteamVR_Controller.GetDeviceIndex(SteamVR_Controller.DeviceRelation.Rightmost);
+                int leftIndex = SteamVR_Controller.GetDeviceIndex(SteamVR_Controller.DeviceRelation.Leftmost);
+
+                // Let's start by finding our controllers
+                foreach (var trackedObject in allTrackedObjects)
+                {
+                    Debug.Log((int)trackedObject.index);
+                    var system = OpenVR.System;
+                    if (system != null && system.GetTrackedDeviceClass((uint)trackedObject.index) == ETrackedDeviceClass.Controller)
+                    {
+                        var device = SteamVR_Controller.Input((int)trackedObject.index);
+                        if (device != null && device.GetPressDown(EVRButtonId.k_EButton_SteamVR_Trigger))
+                        {
+                            rightDetected |= (int)trackedObject.index == rightIndex;
+                            leftDetected |= (int)trackedObject.index == leftIndex;
+                            timerDetectController = 15;
+                        }
+
+                    }
+                }
+
+                if (timerDetectController > 0)
+                {
+                    timerDetectController--;
+                    if (rightDetected && leftDetected)
+                    {
+                        removeIntro = true;
+                        elapsedTime = 0;
+                        timerDetectController = 0;
+                    }
+
+                    if (timerDetectController == 0)
+                    {
+                        rightDetected = false;
+                        leftDetected = false;
+                    }
+                }
+            }
+        }
+
+        /* Removes the intro */
+        if (removeIntro)
+        {
+            if (introVisible)
+            {
+                elapsedTime += Time.deltaTime * 2.1f;
+                if (elapsedTime >= 1)
+                {
+                    tables.SetActive(true);
+                    basketHoop.SetActive(true);
+                    introUi.SetActive(false);
+                    introEnvironment.SetActive(false);
+                    elapsedTime = 1f;
+                    introVisible = false;
+                }
+            }
+            else
+            {
+                elapsedTime -= Time.deltaTime * 2.1f;
+                if (elapsedTime >= 1)
+                {
+                    elapsedTime = 0f;
+                    removeIntro = false;
+                }
+            }
+            updateAlpha(elapsedTime);
+        }
+
+
+    }
+
+    private void updateAlpha(float position)
+    {
+        Color c = fadeRenderer.material.color;
+        c.a = Mathf.Sin((Mathf.PI / 2f) * Mathf.Clamp(position, 0, 1));
+        fadeRenderer.material.color = c;
     }
 }
